db159396cfe17067d0ca1520f3fc85ee69863cc6
HUECYCLES <luhlinard@gmail.com>
2023-12-12 19:24:34 -0300

    changing let timestamps again

--- js/comment-widget.js
+++ js/comment-widget.js
@@ -387,7 +387,7 @@ function createComment(data) {
     let comment = document.createElement('div');
 
     // Get the right timestamps
-    let timestamps = convertTimestamp(data['Carimbo de data/hora']);
+    let timestamps = convertTimestamp(data['Timestamp']);
     let timestamp;
     if (s_longTimestamp) {timestamp = timestamps[0]}
     else {timestamp = timestamps[1]}
